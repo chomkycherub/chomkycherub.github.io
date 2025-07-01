8a070b5bae30085b778a0193a0d024367c901d46
chomkycherub <gvilchezca@gmail.com>
2025-07-01 17:22:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ toggle.addEventListener('click', () => {
     html.setAttribute('data-theme', 'dark');
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.from("section", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    stagger: 0.3,
+    ease: "power2.out"
+  });
+});
+
