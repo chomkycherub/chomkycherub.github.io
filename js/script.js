a337b46074afb5c980972b371c78afcb514ebc7f
chomkycherub <gvilchezca@gmail.com>
2025-07-07 14:32:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ setInterval(() => {
 const projects = [
   {
     title: "Unwound",
-    image: "images/UnwoundGif.gif",
+    image: "images/UnwoundRoomGif.gif",
     description: "Atmospheric stealth-horror game with a dark, cute twist. Play as a wind-up mouse toy through a surreal miniature city full of secrets and dangers as you manage your winding energy.",
     role: "Game, Narrative, Sound Designer, Programmer",
     link: "https://www.youtube.com/watch?v=59f0Bo12WAM",
